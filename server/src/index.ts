c23f991bb0ef6010cdea4903858dd7855eeca0ac
alphazero <alphazero25811@gmail.com>
2021-12-08 18:31:44 +0700

    Apollo Server setup and Hello World

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,37 @@
-import 'dotenv/config';
-import path from 'path';
-import { createConnection } from 'typeorm';
-import { __prod__ } from './constants/constants';
+import "dotenv/config";
+import "reflect-metadata";
+import path from "path";
+import { ApolloServer } from "apollo-server-express";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants/constants";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/Hello";
 
 const main = async () => {
   try {
     await createConnection({
-      type: 'postgres',
+      type: "postgres",
       url: process.env.DATABASE_URL,
       synchronize: !__prod__,
       logging: !__prod__,
-      entities: [path.join(__dirname, 'entity/*.*')],
+      entities: [path.join(__dirname, "entity/*.*")],
     });
+
+    const schema = await buildSchema({
+      resolvers: [HelloResolver],
+    });
+
+    const apolloServer = new ApolloServer({ schema });
+
+    const app = express();
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app });
+
+    const PORT = process.env.PORT || 4000;
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
